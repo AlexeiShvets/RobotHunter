d00810e7fca7d0039e205af2a45889531f60f9ae
AlexeiShvets <arekusei666@gmail.com>
2018-12-11 23:19:17 +0300

    Интерфейс с двумя кнопками move и end

--- RobotHunter/Assets/Scripts/MoveRobot.cs
+++ RobotHunter/Assets/Scripts/MoveRobot.cs
@@ -1,36 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MoveRobot : MonoBehaviour
 {
     [SerializeField]
     private float _moveSpeed = 10f;
-    public Vector3 moveTarget = Vector3.zero;
-    Camera camera;
+
+    public Vector3 moveTarget = Vector3.zero;     
 
     // Use this for initialization
     void Start()
-    {
-        camera = GetComponent<Camera>();
+    {      
         moveTarget = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit, 1000f))
+            if (Physics.Raycast(ray, out hit))
             {
-                Debug.Log("hit");
-                Debug.Log(hit.collider.gameObject.name);
                 if (hit.collider.gameObject.name == "Plane")
                 {
                     moveTarget = hit.point;
+                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.position = moveTarget;
                 }
             }
         }
@@ -40,9 +40,7 @@ public class MoveRobot : MonoBehaviour
             return;
 
         transform.position += transform.forward * Time.deltaTime * _moveSpeed;
-        Quaternion rawRoation = Quaternion.Slerp(transform.rotation,
-                                                 Quaternion.LookRotation(moveTarget - transform.position),
-                                                 _moveSpeed * 0.3f * Time.deltaTime);
+        Quaternion rawRoation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveTarget - transform.position), _moveSpeed  * Time.deltaTime);
         transform.rotation = new Quaternion(0, rawRoation.y, 0, rawRoation.w);
     }
 }
---
+++ RobotHunter/Assets/Scripts/interfaceController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class interfaceController : MonoBehaviour
+{
+
+    public bool EndTurn;
+    public bool IsMove;
+
+    [SerializeField]
+    private GameObject commandPanel;
+
+    private List<Text> texts;
+
+    // Use this for initialization
+    void Start ()
+    {
+        texts = new List<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void ClickEndButton()
+    {
+        EndTurn = true;
+    }
+
+    public void ClickMoveButton()
+    {
+        IsMove = !IsMove;
+        SetCommand("M");
+    }
+
+    public void SetCommand(string strCommand)
+    {
+        if (commandPanel == null || commandPanel.GetComponentsInChildren<Text>().Length>=2)
+            return;
+        var text = Instantiate(Resources.Load("TextCommand")) as GameObject;
+        text.GetComponent<Text>().text = strCommand;
+        if (text != null)
+            text.transform.SetParent(commandPanel.transform);
+
+        
+
+    }
+}
+
