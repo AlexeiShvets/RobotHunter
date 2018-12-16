f03bc394f3e21d4b538349203c59fb9b1b7c829e
AlexeiShvets <arekusei666@gmail.com>
2018-12-17 00:33:06 +0300

    Tactics Movement выделение зоны перемещения

---
+++ RobotHunter/Assets/Scripts/Tactics Movement/MenuScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MenuScript
+{
+
+    [MenuItem("Tools/Assing Tile Material")]
+    public static void AssingTileMaterial()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+        Material material = Resources.Load<Material>("Tile");
+
+        foreach (GameObject tile in tiles)
+        {
+            tile.GetComponent<Renderer>().material = material;
+        }
+    }
+
+    [MenuItem("Tools/Assing Tile Script")]
+    public static void AssingTileScript()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+      
+        foreach (GameObject tile in tiles)
+        {
+            tile.AddComponent<Tile>();
+        }
+    }
+}
